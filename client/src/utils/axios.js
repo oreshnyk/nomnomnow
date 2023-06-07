8be9b94e9efc358ee053818ce86ed0dfdcdbddbc
oreshnyk <55344626+oreshnyk@users.noreply.github.com>
2023-06-07 11:26:36 +0200

    changed baseURL

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://nomnomnow.onrender.com',
+    baseURL: 'https://nomnomnow.onrender.com/api',
 })
 
 instance.interceptors.request.use(config => {
