666beae9617544f2ae6b8d4aa4aed6beb5dcb92c
oreshnyk <55344626+oreshnyk@users.noreply.github.com>
2023-06-07 11:03:24 +0200

    changed baseURL to deploed server

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3002/api',
+    baseURL: 'https://nomnomnow.onrender.com',
 })
 
 instance.interceptors.request.use(config => {
